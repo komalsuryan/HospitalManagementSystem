95e4acb538248c0229077c9166de2670047e8d08
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-25 16:49:39 -0400

    Add code for Community, Doctor and Hospital

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -1,2 +1,44 @@
-package org.example;public class Doctor {
+package org.example;
+
+public class Doctor {
+    private int id;
+    private String name;
+    private String specialization;
+    private int hospitalId;
+
+    public Doctor(int id, String name, String specialization, int hospitalId) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+        this.hospitalId = hospitalId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(int hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
 }
--- src/main/java/org/example/Hospital.java
+++ src/main/java/org/example/Hospital.java
@@ -1,2 +1,44 @@
-package org.example;public class Hospital {
+package org.example;
+
+public class Hospital {
+    private int id;
+    private String Name;
+    private String location;
+    private int communityId;
+
+
+    public Hospital(int id, String name, String location, int communityId) {
+        this.id = id;
+        Name = name;
+        this.location = location;
+        this.communityId = communityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(int communityId) {
+        this.communityId = communityId;
+    }
 }
