84926adc83d7c025304e79f700af5a5980f07768
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-28 01:25:49 +0530

    Add UserTypes enum

---
+++ src/main/java/org/example/UserTypes.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public enum UserTypes {
+    DOCTOR("Doctor"),
+    PATIENT("Patient"),
+    SYSTEM_ADMIN("System Admin"),
+    COMMUNITY_ADMIN("Community Admin");
+
+    private final String userType;
+
+    UserTypes(String userType) {
+        this.userType = userType;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+}
