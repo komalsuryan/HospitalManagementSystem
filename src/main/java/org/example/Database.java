de8b608d93521521bbfccf495cd66c925db44b79
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-28 23:37:47 +0530

    Add functions to create, get, update and remove doctors

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -3,9 +3,7 @@ package org.example;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import javax.print.Doc;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -23,6 +21,9 @@ public class Database {
             }
         }
 
+        // initialize gson
+        gson = new Gson();
+
         // create the doctors.json file if it does not exist
         File doctorsFile = new File(PATH + "doctors.json");
         if (!doctorsFile.exists()) {
@@ -30,36 +31,32 @@ public class Database {
                 if(!doctorsFile.createNewFile()) {
                     throw new RuntimeException("Could not create file: " + PATH + "doctors.json");
                 }
+                FileIO.writeFile("doctors", "[]");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-        // initialize gson
-        gson = new Gson();
     }
 
-    public void addObject(UserTypes type, Object object) {
-        if (type == UserTypes.DOCTOR) {
-            // add doctor to doctors.json
-        } else if (type == UserTypes.PATIENT) {
-            // add patient to patients.json
-        } else if (type == UserTypes.SYSTEM_ADMIN) {
-            // add system admin to system_admins.json
-        } else if (type == UserTypes.COMMUNITY_ADMIN) {
-            // add community admin to community_admins.json
-        }
-    }
-
-    public ArrayList<Doctor> getAllDoctors() throws FileNotFoundException {
+    public ArrayList<Doctor> getAllDoctors() throws IOException {
         // get contents of doctors file as a string
         String contents = FileIO.readFile("doctors");
-        System.out.println(contents);
-        if (contents == null || contents.isEmpty()) {
+        if (contents.isEmpty() || contents.equals("null")) {
+            System.out.println("contents is null or empty");
             return new ArrayList<>();
         }
         Type doctorListType = new TypeToken<ArrayList<Doctor>>(){}.getType();
         return gson.fromJson(contents, doctorListType);
+    }
+
+    public Doctor getDoctor(int id) throws IOException {
+        ArrayList<Doctor> doctors = getAllDoctors();
+        for (Doctor doctor : doctors) {
+            if (doctor.getId() == id) {
+                return doctor;
+            }
+        }
+        return null;
     }
 
     public void addDoctor(Doctor doctor) throws IOException {
@@ -71,8 +68,26 @@ public class Database {
         FileIO.writeFile("doctors", gson.toJson(doctors));
     }
 
-    public static void main(String[] args) throws IOException {
-        Database db = new Database();
-        db.addDoctor(new Doctor(1, "Suyash Saxena", "Ornithology", 1));
+    public void removeDoctor(int id) throws IOException {
+        // get all doctors
+        ArrayList<Doctor> doctors = getAllDoctors();
+        // remove doctor with id
+        doctors.removeIf(doctor -> doctor.getId() == id);
+        // write to file
+        FileIO.writeFile("doctors", gson.toJson(doctors));
+    }
+
+    public void updateDoctor(Doctor doctor) throws IOException {
+        // get all doctors
+        ArrayList<Doctor> doctors = getAllDoctors();
+        // update doctor
+        for (int i = 0; i < doctors.size(); i++) {
+            if (doctors.get(i).getId() == doctor.getId()) {
+                doctors.set(i, doctor);
+                break;
+            }
+        }
+        // write to file
+        FileIO.writeFile("doctors", gson.toJson(doctors));
     }
 }
--- src/main/java/org/example/FileIO.java
+++ src/main/java/org/example/FileIO.java
@@ -4,20 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileIO {
     private static final String PATH = System.getProperty("user.home") + "/.hms/";
-    public static String readFile(String fileName) throws FileNotFoundException {
-        // read file
-        File file = new File(PATH + fileName + ".json");
-        JsonReader reader = new JsonReader(new FileReader(file));
-        return reader.toString();
+    public static String readFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(PATH + fileName + ".json")));
     }
 
     public static void writeFile(String fileName, String contents) throws IOException {
-        // write to file
-        FileWriter file = new FileWriter(PATH + fileName + ".json");
-        // write the contents string to the file overwriting the previous contents
-        new Gson().toJson(contents, file);
+        FileWriter fileWriter = new FileWriter(PATH + fileName + ".json");
+        fileWriter.write(contents);
+        fileWriter.close();
     }
 }
