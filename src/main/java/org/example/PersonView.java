5c0f89a596a29a771c6d4f2721ab14eaf9e97bca
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-30 08:27:05 +0530

    (WIP) Add Find Doctors block

--- src/main/java/org/example/PersonView.java
+++ src/main/java/org/example/PersonView.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.personViewUiElements.PersonDoctorBlock;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PersonView {
     private JLabel welcomeLabel;
@@ -10,15 +14,36 @@ public class PersonView {
     private JPanel personViewMainPanel;
     private JPanel personViewHeadingPanel;
     private JLabel findDoctorsLabel;
+    private JScrollPane findDoctorsScrollPane;
+    private JPanel findDoctorsPanel;
+    private JPanel findDoctorsTopPanel;
+    private JPanel findDoctorsSearchPanel;
+    private JLabel findDoctorsSearchLabel;
+    private JTextField findDoctorsSearchText;
+    private Database db;
 
-    public PersonView(int id) {
+    public PersonView(int id) throws IOException {
+        db = new Database();
         welcomeLabel.setText("Welcome, " + id);
-        editPersonButton.setText("Edit");
+        editPersonButton.setText("Edit your details");
         logoutPersonButton.setText("Logout");
         findDoctorsLabel.setText("Doctors in your community");
+        findDoctorsSearchLabel.setText("Search");
+        createDoctorBlocks();
     }
 
-    public static void main(String[] args) {
+    private void createDoctorBlocks() throws IOException {
+        ArrayList<Doctor> doctors = db.getAllDoctors();
+        findDoctorsPanel.setLayout(new BoxLayout(findDoctorsPanel, BoxLayout.X_AXIS));
+        for (Doctor doctor : doctors) {
+            PersonDoctorBlock doctorBlock = new PersonDoctorBlock(doctor);
+            findDoctorsPanel.add(doctorBlock.getMainPanel());
+        }
+        findDoctorsPanel.revalidate();
+        findDoctorsPanel.repaint();
+    }
+
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("PersonView");
         frame.setContentPane(new PersonView(1).personViewMainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
