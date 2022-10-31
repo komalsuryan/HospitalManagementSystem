b4dc5be84d83030b15502953ad01b14103f40cb3
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-31 08:04:41 +0530

    Fix communityId to int from String

--- src/main/java/org/komalsuryan/Patient.java
+++ src/main/java/org/komalsuryan/Patient.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Patient extends Person {
     private final int id;
     private static final AtomicInteger count = new AtomicInteger(new Database().getAllPatients().size());
-    public Patient(String ssNumber, String name, String communityId, LocalDate dateOfBirth, String sex, float height, float weight) {
+    public Patient(String ssNumber, String name, int communityId, LocalDate dateOfBirth, String sex, float height, float weight) {
         super(ssNumber, name, communityId, dateOfBirth, sex, height, weight);
         this.id = count.incrementAndGet();
     }
