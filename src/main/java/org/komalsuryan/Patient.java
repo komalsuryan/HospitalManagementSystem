5961ec1738e61b4aa3f338aa8b4f32f55d4e24f8
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-10-31 16:08:10 +0530

    Add email and password to Patient

--- src/main/java/org/komalsuryan/Patient.java
+++ src/main/java/org/komalsuryan/Patient.java
@@ -5,13 +5,40 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Patient extends Person {
     private final int id;
+    private String email;
+    private String password;
     private static final AtomicInteger count = new AtomicInteger(new Database().getAllPatients().size());
-    public Patient(String ssNumber, String name, int communityId, LocalDate dateOfBirth, String sex, float height, float weight) {
+    public Patient(String ssNumber, String name, int communityId, LocalDate dateOfBirth, String sex, float height, float weight, String email, String password) {
         super(ssNumber, name, communityId, dateOfBirth, sex, height, weight);
         this.id = count.incrementAndGet();
+        this.email = email;
+        this.password = password;
+    }
+
+    public Patient(Person person, String email, String password) {
+        super(person);
+        this.id = count.incrementAndGet();
+        this.email = email;
+        this.password = password;
     }
 
     public int getId() {
         return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
